4c3de8ce99f5950f953c6506e98952be62738a30
javasergey <simracing12@mail.ru>
2019-06-11 18:45:44 +0300

    add photos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Router, Route, Link, Switch } from "react-router-dom";
 import createBrowserHistory from "history/createBrowserHistory";
 import { Table, Button } from 'antd';
 import "antd/dist/antd.css";
+import logo1 from "./img/1.jpg";
+import logo2 from "./img/2.jpg";
+import logo3 from "./img/3.jpg";
 
 const url = 'http://localhost:3001';
 const history = createBrowserHistory();
@@ -42,6 +45,7 @@ const Author1 = () => {
 	return (
 
 		<div>
+			<img src={logo1} alt={"logo1"} width={"200px"}/>
 			<h3>Лев Николаевич Толстой</h3>
 			<p><b>Дата рождения:</b>28.08.1828</p>
 			<p><b>Дата смерти:</b>07.11.1910</p>
@@ -53,6 +57,7 @@ const Author2 = () => {
 	return (
 
 		<div>
+			<img src={logo2} alt={"logo2"} width={"200px"}/>
 			<h3>Алан Александр Милн</h3>
 			<p><b>Дата рождения:</b>18.01.1882</p>
 			<p><b>Дата смерти:</b>31.01.1956</p>
@@ -64,6 +69,7 @@ const Author3 = () => {
 	return (
 
 		<div>
+			<img src={logo3} alt={"logo3"} width={"200px"}/>
 			<h3>Поляков Илья Сергеевич</h3>
 			<p><b>Дата рождения:</b>19.12.1954</p>
 			<p><b>Дата смерти:</b>13.06.2015</p>
