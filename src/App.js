2d5ca2872716eaf9e9693b6349ec99338f81c8a0
javasergey <simracing12@mail.ru>
2019-06-21 16:20:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,101 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, Link, Switch } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
-import { Table, Button } from 'antd';
+import React, { Component } from "react";
+import { Router, Route, Switch, Link } from "react-router-dom";
+import { Table } from "antd";
 import "antd/dist/antd.css";
+import { createBrowserHistory } from "history";
 
-const url = 'http://localhost:3001';
 const history = createBrowserHistory();
 
 const columns = [
-  {
-    title: 'Наименование книги',
-    dataIndex: 'bookname',
-    key: '1',
-  },
-  {
-    title: 'Авторы',
-    dataIndex: 'author',
-    key: '2',
-     render: (name, id) => <Link to={'/authors/'+id.key}>{name}</Link>,
-  },
-  {
-    title: 'Дата',
-    dataIndex: 'date',
-    key: '3',
-  }
+	{
+		title: "Наименование книги",
+		dataIndex: "bookname",
+		key: "1"
+	},
+	{
+		title: "Авторы",
+		dataIndex: "author",
+		key: "2",
+		render: (text, record) => (<Link to={"/authors/" + record.authorid}>{text}</Link>)
+	},
+	{
+		title: "Дата",
+		dataIndex: "date",
+		key: "3"
+	}
 ];
 
-const Authors = () => (
-  <Switch>
-    <Route path="/authors/1" component={Author1}/>
-    <Route path="/authors/2" component={Author2}/>
-    <Route path="/authors/3" component={Author3}/>
-  </Switch>
-)
 
-const Author1 = () => {
-	return (
-
-		<div>
-			<img src={'http://localhost:3001/exp1.jpg'} alt={"logo1"} width={"150px"}/>
-			<h3>Лев Николаевич Толстой</h3>
-			<p><b>Дата рождения:</b>28.08.1828</p>
-			<p><b>Дата смерти:</b>07.11.1910</p>
-			<Link to={'/'}><b>BACK</b></Link>
-		</div>
-	)
-}
-const Author2 = () => {
-	return (
-
-		<div>
-			<img src={"http://localhost:3001/exp2.jpg"} alt={"logo2"} width={"150px"}/>
-			<h3>Алан Александр Милн</h3>
-			<p><b>Дата рождения:</b>18.01.1882</p>
-			<p><b>Дата смерти:</b>31.01.1956</p>
-			<Link to={'/'}><b>BACK</b></Link>
-		</div>
-	)
-}
-const Author3 = () => {
-	return (
-
-		<div>
-			<img src={"http://localhost:3001/exp3.jpg"} alt={"logo3"} width={"150px"}/>
-			<h3>Поляков Илья Сергеевич</h3>
-			<p><b>Дата рождения:</b>19.12.1954</p>
-			<p><b>Дата смерти:</b>13.06.2015</p>
-		 	<Link to={'/'}><b>BACK</b></Link>
-		</div>
-	)
-}
-
-class ContentFeed extends React.Component {
-	constructor(){
-		super()
-		this.state = {
-			items:[]
-		}
-		 
+class BooksTable extends Component {
+	render() {
+		return (
+			<Table columns={columns} dataSource={this.props.items} />
+		);
 	}
-	
-	componentDidMount(){
+}
+
+
+class Author extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		const authorId = this.props.match.params.authorId;
+		this.getAuthor(authorId);
+	}
+
+	getAuthor(authorId) {
+		fetch("http://localhost:3001/authors/" + authorId)
+			.then(response => response.json())
+			.then(data => this.setState(data))
+	}
+
+	render() {
+		const { name, dateOfBirth, dateOfDeath, photo } = this.state;
+		return (
+			<div>
+				{
+					<div>
+						<img src={'http://localhost:3001' + photo} alt={name} width="150" />
+						<h3>{name}</h3>
+						<p><b>Дата рождения:</b> {dateOfBirth}</p>
+						<p><b>Дата смерти:</b> {dateOfDeath}</p>
+						<Link to={"/"}><b>BACK</b></Link>
+					</div>
+				}
+			</div>
+		);
+	}
+}
+
+class ContentFeed extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: []
+		};
+	}
+
+	componentDidMount() {
 		this.getItems();
 	}
-	getItems(){
-		fetch(url)
-			.then(response => response.json())
-			.then(response => this.setState({items: response}))
-	}
-	render(){
 
+	getItems() {
+		fetch("http://localhost:3001/books")
+			.then(response => response.json())
+			.then(data => this.setState({ items: data }))
+	}
+
+	render() {
 		return (
 			<Router history={history}>
-				<Table columns={columns} dataSource={this.state.items}/>
-
-				<Authors />
+				<Switch>
+					<Route path="/" exact render={() => (<BooksTable items={this.state.items} />)} />
+					<Route path="/authors/:authorId" component={Author} />
+				</Switch>
 			</Router>
 		);
 	}
--- src/express.js
+++ src/express.js
@@ -1,16 +1,83 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require('path');
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+const booksData = [{
+	"key": "1",
+	"bookname": "Война и мир",
+	"authorid": 1,
+	"author": "Лев Николаевич Толстой",
+	"date": "31.04.1867",
+	"src": "/exp1.jpg"
+}, {
+	"key": "2",
+	"bookname": "Винни Пух",
+	"authorid": 2,
+	"author": "Алн Александр Милн",
+	"date": "01.10.1959",
+	"src": "/exp2.jpg"
+}, {
+	"key": "3",
+	"bookname": "Печи для дачи своими руками",
+	"authorid": 3,
+	"author": "Поляков Илья Сергеевич",
+	"date": "21.07.2010",
+	"src": "/exp3.jpg"
+},
+	{
+	"key": "4",
+	"bookname": "Тесты по программированию",
+	"authorid": 8,
+	"author": "Тест Петр Сергеевич",
+	"date": "21.07.2010",
+	"src": "http://localhost:3001/exp3.jpg"
+	}
+];
+
+const authorsData = [{
+	authorId: 1,
+	name: "Лев Николаевич Толстой",
+	dateOfBirth: "28.08.1828",
+	dateOfDeath: "07.11.1910",
+	photo: "/exp1.jpg"
+},  {
+	authorId: 2,
+	name: "Алан Александр Милн",
+	dateOfBirth: "18.01.188",
+	dateOfDeath: "31.01.1956",
+	photo: "/exp2.jpg"
+},  {
+	authorId: 3,
+	name: "Поляков Илья Сергеевич",
+	dateOfBirth: "19.12.1954",
+	dateOfDeath: "13.06.2015",
+	photo: "/exp3.jpg"
+},  {
+	authorId: 8,
+	name: "Тест Петр Сергеевич",
+	dateOfBirth: "19.12.1954",
+	dateOfDeath: "13.06.2015",
+	photo: "/exp3.jpg"
+	}
+];
+
+
+app.all("/*", function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
 });
 
-app.use('', express.static('img'));
+app.use('', express.static(path.resolve(__dirname, './img')));
 
-app.get('/', function(req, res){
-	res.json([{"key":"1","bookname":"Война и мир","author":"Лев Николаевич Толстой","date":"31.04.1867","src":"http://localhost:3001/exp1.jpg"},{"key":"2","bookname":"Винни Пух","author":"Алан Александр Милн","date":"01.10.1959","src":"http://localhost:3001/exp2.jpg"},{"key":"3","bookname":"Печи для дачи своими руками","author":"Поляков Илья Сергеевич","date":"21.07.2010","src":"http://localhost:3001/exp3.jpg"}]);
+app.get("/books", function (req, res) {
+	res.json(booksData);
+});
+
+app.get("/authors/:authorId", function (req, res) {
+	const authorId = parseInt(req.params.authorId);
+	const author = authorsData.find((item) => (item.authorId === authorId));
+	res.json(author);
 });
 
 app.listen(3001);
